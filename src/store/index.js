edc8449075f6d3fd2852e0a2881d23d9023e2056
Hanna <joysyr@gmail.com>
2018-02-05 18:04:52 +0800

    improve(common):add vuex to project

---
+++ src/store/index.js
@@ -0,0 +1,16 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import lists from './modules/lists';
+import items from './modules/items';
+
+Vue.use(Vuex);
+
+const debug = process.env.NODE_ENV !== 'production';
+
+export default new Vuex.Store({
+  modules: {
+    lists,
+    items,
+  },
+  strict: debug,
+});
