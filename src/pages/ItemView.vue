bd8f06cf0b983298958e84b1823b13658637bf51
Hanna <joysyr@gmail.com>
2018-02-11 11:03:53 +0800

    feature(itemView):add route for itemView, show all items in itemView page

---
+++ src/pages/ItemView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <header-component />
+    <ul v-for="(item, index) in items" :key="index">
+      <li>{{ item.title.rendered }}</li>
+    </ul>
+  </div>
+</template>
+<script>
+import { mapGetters } from 'vuex';
+import HeaderComponent from '../components/partial/Header';
+
+export default {
+  name: 'ItemView',
+  components: {
+    HeaderComponent,
+  },
+  data() {
+    return {
+      path: this.$route.name,
+    };
+  },
+  computed: {
+    ...mapGetters({
+      items: 'allItems',
+    }),
+  },
+  mounted() {
+    this.$store.dispatch('fetchAllItems');
+  },
+};
+</script>
